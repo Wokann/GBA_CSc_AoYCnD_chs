e02a2d40626c8c20f662eea21111eb5a1d477a3f
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-12-04 12:41:14 +0800

    c代码有符号转无符号

--- src/RenderText.c
+++ src/RenderText.c
@@ -32,49 +32,50 @@ typedef u8  bool8;
 typedef u16 bool16;
 typedef u32 bool32;
 
-void RenderText_08009F3C(s32 a1) 
+void RenderText_08009F3C(u32 a1) 
 {
-    if ( *(s16 *)(a1 + 2) ) 
+    if ( *(u16 *)(a1 + 2) ) 
     {
-        s8 v2 = *(s8 *)(a1 + 0x12);
+        u8 v2 = *(u8 *)(a1 + 0x12);
         if ( (v2 & 1) == 0 ) 
         {
-            *(s8 *)(a1 + 0x12) = v2 | 1; 
-            s32 v3 = *(u16 *)(a1 + 2);
-            s16 *v4 = (s16 *)(*(s32 *)(a1 + 4) + 2 * v3 - 2);
-            s32 dest = *(s32 *)(a1 + 8) + 0x20 * (*(u16 *)(a1 + 0x10) - (2 * (v3 - 1) + 1));
+            *(u8 *)(a1 + 0x12) = v2 | 1; 
+            u32 v3 = *(u16 *)(a1 + 2);
+            u16 *v4 = (u16 *)(*(u32 *)(a1 + 4) + 2 * v3 - 2);
+            u32 dest = *(u32 *)(a1 + 8) + 0x20 * (*(u16 *)(a1 + 0x10) - (2 * (v3 - 1) + 1));
             u32 v6 = 0;
             
-            if ( *(s16 *)(a1 + 2) ) 
+            if ( *(u16 *)(a1 + 2) ) 
             {
-                s32 v11;
+                u32 v11;
                 do {
-                    s16 curChar = *v4;
+                    u16 curChar = *v4;
                     if (curChar) 
                     {
-                        s32 v8 = (u16)(curChar & 0x0) >> 9;
-                        //原程序s32 v8 = (u16)(curChar & 0xE00) >> 9;
-                        s32 baseOffesetOfFont = *(u32 *)(0x848E92C + v8); 
+                        u32 v8 = (u16)(curChar & 0x0) >> 9;
+                        //原程序u32 v8 = (u16)(curChar & 0xE00) >> 9;
+                        u32 baseOffesetOfFont = *(u32 *)(0x848E92C + v8); 
                         u16 *v10 = *(u16 **)(4 * (curChar & 0x1FF) + *(u32 *)(0x848E930 + v8));
                         
                         if ( *(u16 *)(0x848E934 + v8) ) 
                         {
-                            *(s32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[1];
-                            *(s32 *) 0x40000D8 = dest;
-                            *(s32 *) 0x40000DC = 0x80000010;
-                            *(s32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[3];
-                            *(s32 *) 0x40000D8 = dest + 0x20;
-                            *(s32 *) 0x40000DC = 0x80000010;
-                            *(s32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[0];
-                            *(s32 *) 0x40000D8 = dest + 0x40;
-                            *(s32 *) 0x40000DC = 0x80000010;
-                            *(s32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[2];
-                            *(s32 *) 0x40000D8 = dest + 0x60;
-                            *(s32 *) 0x40000DC = 0x80000010;
+                            *(u32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[1];
+                            *(u32 *) 0x40000D8 = dest;
+                            *(u32 *) 0x40000DC = 0x80000010;
+                            *(u32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[3];
+                            *(u32 *) 0x40000D8 = dest + 0x20;
+                            *(u32 *) 0x40000DC = 0x80000010;
+                            *(u32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[0];
+                            *(u32 *) 0x40000D8 = dest + 0x40;
+                            *(u32 *) 0x40000DC = 0x80000010;
+                            *(u32 *) 0x40000D4 = baseOffesetOfFont + 0x20 * v10[2];
+                            *(u32 *) 0x40000D8 = dest + 0x60;
+                            *(u32 *) 0x40000DC = 0x80000010;
                             dest += 0x80;
                         } 
                         else 
                         {   
+                            //汉字识别逻辑
                             u32 offset;
                             u8 hi = curChar >> 8;
                             u8 lo = curChar & 0xFF;
@@ -93,9 +94,10 @@ void RenderText_08009F3C(s32 a1)
                             else
                                 offset = v10[0];
                             
-                            *(s32 *) 0x40000D4 = baseOffesetOfFont + 0x40 * offset;
-                            *(s32 *) 0x40000D8 = dest;
-                            *(s32 *) 0x40000DC = 0x80000020;
+                            //原程序*(u32 *) 0x40000D4 = baseOffesetOfFont + (v10[0] << 6);
+                            *(u32 *) 0x40000D4 = baseOffesetOfFont + 0x40 * offset;
+                            *(u32 *) 0x40000D8 = dest;
+                            *(u32 *) 0x40000DC = 0x80000020;
                             dest += 0x40;
                         }
                     }
