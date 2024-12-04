cc5050bd0238ae8ae95f2ceb39ca4e22ced088e8
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-12-04 22:58:50 +0800

    编译指令更新

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
  cd /d %~dp0
- C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc.exe -mabi=aapcs -std=c99 -w -nostartfiles -nodefaultlibs -Os -march=armv5te -mtune=arm946e-s -mthumb -Wl,--use-blx -I C:\devkitPro\libgba\include -c src\RenderText.c -o build\RenderText.o
- C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc.exe -mabi=aapcs -std=c99 -w -nostartfiles -nodefaultlibs -Os -march=armv5te -mtune=arm946e-s -mthumb -Wl,--use-blx -I C:\devkitPro\libgba\include -S src\RenderText.c -o build\RenderText.s
+ C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc.exe -mabi=aapcs -std=c99 -w -nostartfiles -nodefaultlibs -O2 -march=armv5te -mtune=arm946e-s -mthumb -I C:\devkitPro\libgba\include -c src\RenderText.c -o build\RenderText.o
+ C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc.exe -mabi=aapcs -std=c99 -w -nostartfiles -nodefaultlibs -O2 -march=armv5te -mtune=arm946e-s -mthumb -I C:\devkitPro\libgba\include -S src\RenderText.c -o build\RenderText.s
  C:\devkitPro\devkitARM\bin\arm-none-eabi-objcopy.exe -O binary build\RenderText.o build\RenderText.bin
 
-.\tools\armips\armips.exe main.s -sym main.sym
+.\tools\armips\armips.exe main.s -sym CSc_AoYCnD_chs.sym
 @echo off
 ping 127.0.0.1 -n 3 >nul
 
- rem tools/agbcc/bin/agbcc  -mthumb-interwork -Wimplicit -Wparentheses -Werror -O1 -g -fhex-asm src/RenderText.c -o build/RenderText.o
-rem tools/agbcc/bin/agbcc -mthumb-interwork -Wimplicit -Wparentheses -Werror -O1 -g -fhex-asm src/RenderText.c -o build/RenderText.s
-rem arm-none-eabi-objcopy -O binary build/RenderText.o build/RenderText.bin
+ rem tools\agbcc\bin\agbcc  -mthumb-interwork -Wimplicit -Wparentheses -Werror -O1 -g -fhex-asm src\RenderText.c -o build\RenderText.o
+rem tools\agbcc\bin\agbcc -mthumb-interwork -Wimplicit -Wparentheses -Werror -O1 -g -fhex-asm src\RenderText.c -o build\RenderText.s
+rem arm-none-eabi-objcopy -O binary build\RenderText.o build\RenderText.bin
