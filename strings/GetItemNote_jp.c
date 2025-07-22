239703cfe7c795485017267bf93a0992eea7846a
GitHub Wokann <Kenny_chen1998@outlook.com>
2025-07-22 13:55:59 +0800

    日文文本（待修复

---
+++ strings/GetItemNote_jp.c
@@ -0,0 +1,360 @@
+#include "libgba\gba_types.h"
+#include "strings\GetItemNote.h"
+
+// text 0x080BC678-0x080BDE9D
+u8 GetItemNote_000_000[] = _("コレクションナンバ— 1 のカ—ドを{End}");
+u8 GetItemNote_000_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_001_000[] = _("コレクションナンバ— 2 のカ—ドを{End}");
+u8 GetItemNote_001_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_002_000[] = _("コレクションナンバ— 3 のカ—ドを{End}");
+u8 GetItemNote_002_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_003_000[] = _("コレクションナンバ— 4 のカ—ドを{End}");
+u8 GetItemNote_003_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_004_000[] = _("コレクションナンバ— 5 のカ—ドを{End}");
+u8 GetItemNote_004_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_005_000[] = _("コレクションナンバ— 6 のカ—ドを{End}");
+u8 GetItemNote_005_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_006_000[] = _("コレクションナンバ— 7 のカ—ドを{End}");
+u8 GetItemNote_006_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_007_000[] = _("コレクションナンバ— 8 のカ—ドを{End}");
+u8 GetItemNote_007_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_008_000[] = _("コレクションナンバ— 9 のカ—ドを{End}");
+u8 GetItemNote_008_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_009_000[] = _("コレクションナンバ— 10 のカ—ドを{End}");
+u8 GetItemNote_009_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_010_000[] = _("コレクションナンバ— 11 のカ—ドを{End}");
+u8 GetItemNote_010_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_011_000[] = _("コレクションナンバ— 12 のカ—ドを{End}");
+u8 GetItemNote_011_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_012_000[] = _("コレクションナンバ— 13 のカ—ドを{End}");
+u8 GetItemNote_012_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_013_000[] = _("コレクションナンバ— 14 のカ—ドを{End}");
+u8 GetItemNote_013_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_014_000[] = _("コレクションナンバ— 15 のカ—ドを{End}");
+u8 GetItemNote_014_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_015_000[] = _("コレクションナンバ— 16 のカ—ドを{End}");
+u8 GetItemNote_015_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_016_000[] = _("コレクションナンバ— 17 のカ—ドを{End}");
+u8 GetItemNote_016_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_017_000[] = _("コレクションナンバ— 18 のカ—ドを{End}");
+u8 GetItemNote_017_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_018_000[] = _("コレクションナンバ— 19 のカ—ドを{End}");
+u8 GetItemNote_018_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_019_000[] = _("コレクションナンバ— 20 のカ—ドを{End}");
+u8 GetItemNote_019_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_020_000[] = _("コレクションナンバ— 21 のカ—ドを{End}");
+u8 GetItemNote_020_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_021_000[] = _("コレクションナンバ— 22 のカ—ドを{End}");
+u8 GetItemNote_021_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_022_000[] = _("コレクションナンバ— 23 のカ—ドを{End}");
+u8 GetItemNote_022_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_023_000[] = _("コレクションナンバ— 24 のカ—ドを{End}");
+u8 GetItemNote_023_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_024_000[] = _("コレクションナンバ— 25 のカ—ドを{End}");
+u8 GetItemNote_024_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_025_000[] = _("コレクションナンバ— 26 のカ—ドを{End}");
+u8 GetItemNote_025_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_026_000[] = _("コレクションナンバ— 27 のカ—ドを{End}");
+u8 GetItemNote_026_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_027_000[] = _("コレクションナンバ— 28 のカ—ドを{End}");
+u8 GetItemNote_027_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_028_000[] = _("コレクションナンバ— 29 のカ—ドを{End}");
+u8 GetItemNote_028_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_029_000[] = _("コレクションナンバ— 30 のカ—ドを{End}");
+u8 GetItemNote_029_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_030_000[] = _("コレクションナンバ— 31 のカ—ドを{End}");
+u8 GetItemNote_030_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_031_000[] = _("コレクションナンバ— 32 のカ—ドを{End}");
+u8 GetItemNote_031_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_032_000[] = _("コレクションナンバ— 33 のカ—ドを{End}");
+u8 GetItemNote_032_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_033_000[] = _("コレクションナンバ— 34 のカ—ドを{End}");
+u8 GetItemNote_033_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_034_000[] = _("コレクションナンバ— 35 のカ—ドを{End}");
+u8 GetItemNote_034_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_035_000[] = _("コレクションナンバ— 36 のカ—ドを{End}");
+u8 GetItemNote_035_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_036_000[] = _("コレクションナンバ— 37 のカ—ドを{End}");
+u8 GetItemNote_036_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_037_000[] = _("コレクションナンバ— 38 のカ—ドを{End}");
+u8 GetItemNote_037_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_038_000[] = _("コレクションナンバ— 39 のカ—ドを{End}");
+u8 GetItemNote_038_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_039_000[] = _("コレクションナンバ— 40 のカ—ドを{End}");
+u8 GetItemNote_039_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_040_000[] = _("コレクションナンバ— 41 のカ—ドを{End}");
+u8 GetItemNote_040_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_041_000[] = _("コレクションナンバ— 42 のカ—ドを{End}");
+u8 GetItemNote_041_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_042_000[] = _("コレクションナンバ— 43 のカ—ドを{End}");
+u8 GetItemNote_042_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_043_000[] = _("コレクションナンバ— 44 のカ—ドを{End}");
+u8 GetItemNote_043_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_044_000[] = _("コレクションナンバ— 45 のカ—ドを{End}");
+u8 GetItemNote_044_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_045_000[] = _("コレクションナンバ— 46 のカ—ドを{End}");
+u8 GetItemNote_045_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_046_000[] = _("コレクションナンバ— 47 のカ—ドを{End}");
+u8 GetItemNote_046_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_047_000[] = _("コレクションナンバ— 48 のカ—ドを{End}");
+u8 GetItemNote_047_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_048_000[] = _("コレクションナンバ— 49 のカ—ドを{End}");
+u8 GetItemNote_048_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_049_000[] = _("コレクションナンバ— 50 のカ—ドを{End}");
+u8 GetItemNote_049_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_050_000[] = _("コレクションナンバ— 51 のカ—ドを{End}");
+u8 GetItemNote_050_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_051_000[] = _("コレクションナンバ— 52 のカ—ドを{End}");
+u8 GetItemNote_051_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_052_000[] = _("コレクションナンバ— 53 のカ—ドを{End}");
+u8 GetItemNote_052_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_053_000[] = _("コレクションナンバ— 54 のカ—ドを{End}");
+u8 GetItemNote_053_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_054_000[] = _("コレクションナンバ— 55 のカ—ドを{End}");
+u8 GetItemNote_054_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_055_000[] = _("コレクションナンバ— 56 のカ—ドを{End}");
+u8 GetItemNote_055_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_056_000[] = _("コレクションナンバ— 57 のカ—ドを{End}");
+u8 GetItemNote_056_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_057_000[] = _("コレクションナンバ— 58 のカ—ドを{End}");
+u8 GetItemNote_057_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_058_000[] = _("コレクションナンバ— 59 のカ—ドを{End}");
+u8 GetItemNote_058_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_059_000[] = _("コレクションナンバ— 60 のカ—ドを{End}");
+u8 GetItemNote_059_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_060_000[] = _("コレクションナンバ— 61 のカ—ドを{End}");
+u8 GetItemNote_060_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_061_000[] = _("コレクションナンバ— 62 のカ—ドを{End}");
+u8 GetItemNote_061_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_062_000[] = _("コレクションナンバ— 63 のカ—ドを{End}");
+u8 GetItemNote_062_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_063_000[] = _("コレクションナンバ— 64 のカ—ドを{End}");
+u8 GetItemNote_063_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_064_000[] = _("コレクションナンバ— 65 のカ—ドを{End}");
+u8 GetItemNote_064_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_065_000[] = _("コレクションナンバ— 66 のカ—ドを{End}");
+u8 GetItemNote_065_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_066_000[] = _("コレクションナンバ— 67 のカ—ドを{End}");
+u8 GetItemNote_066_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_067_000[] = _("コレクションナンバ— 68 のカ—ドを{End}");
+u8 GetItemNote_067_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_068_000[] = _("コレクションナンバ— 69 のカ—ドを{End}");
+u8 GetItemNote_068_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_069_000[] = _("コレクションナンバ— 70 のカ—ドを{End}");
+u8 GetItemNote_069_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_070_000[] = _("コレクションナンバ— 71 のカ—ドを{End}");
+u8 GetItemNote_070_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_071_000[] = _("コレクションナンバ— 72 のカ—ドを{End}");
+u8 GetItemNote_071_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_072_000[] = _("コレクションナンバ— 73 のカ—ドを{End}");
+u8 GetItemNote_072_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_073_000[] = _("コレクションナンバ— 74 のカ—ドを{End}");
+u8 GetItemNote_073_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_074_000[] = _("コレクションナンバ— 75 のカ—ドを{End}");
+u8 GetItemNote_074_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_075_000[] = _("コレクションナンバ— 76 のカ—ドを{End}");
+u8 GetItemNote_075_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_076_000[] = _("コレクションナンバ— 77 のカ—ドを{End}");
+u8 GetItemNote_076_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_077_000[] = _("コレクションナンバ— 78 のカ—ドを{End}");
+u8 GetItemNote_077_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_078_000[] = _("コレクションナンバ— 79 のカ—ドを{End}");
+u8 GetItemNote_078_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_079_000[] = _("コレクションナンバ— 80 のカ—ドを{End}");
+u8 GetItemNote_079_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_080_000[] = _("コレクションナンバ— 81 のカ—ドを{End}");
+u8 GetItemNote_080_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_081_000[] = _("コレクションナンバ— 82 のカ—ドを{End}");
+u8 GetItemNote_081_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_082_000[] = _("コレクションナンバ— 83 のカ—ドを{End}");
+u8 GetItemNote_082_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_083_000[] = _("コレクションナンバ— 84 のカ—ドを{End}");
+u8 GetItemNote_083_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_084_000[] = _("コレクションナンバ— 85 のカ—ドを{End}");
+u8 GetItemNote_084_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_085_000[] = _("コレクションナンバ— 86 のカ—ドを{End}");
+u8 GetItemNote_085_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_086_000[] = _("コレクションナンバ— 87 のカ—ドを{End}");
+u8 GetItemNote_086_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_087_000[] = _("コレクションナンバ— 88 のカ—ドを{End}");
+u8 GetItemNote_087_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_088_000[] = _("コレクションナンバ— 89 のカ—ドを{End}");
+u8 GetItemNote_088_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_089_000[] = _("コレクションナンバ— 90 のカ—ドを{End}");
+u8 GetItemNote_089_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_090_000[] = _("コレクションナンバ— 91 のカ—ドを{End}");
+u8 GetItemNote_090_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_091_000[] = _("コレクションナンバ— 92 のカ—ドを{End}");
+u8 GetItemNote_091_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_092_000[] = _("コレクションナンバ— 93 のカ—ドを{End}");
+u8 GetItemNote_092_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_093_000[] = _("コレクションナンバ— 94 のカ—ドを{End}");
+u8 GetItemNote_093_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_094_000[] = _("コレクションナンバ— 95 のカ—ドを{End}");
+u8 GetItemNote_094_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_095_000[] = _("コレクションナンバ— 96 のカ—ドを{End}");
+u8 GetItemNote_095_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_096_000[] = _("コレクションナンバ— 97 のカ—ドを{End}");
+u8 GetItemNote_096_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_097_000[] = _("コレクションナンバ— 98 のカ—ドを{End}");
+u8 GetItemNote_097_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_098_000[] = _("コレクションナンバ— 99 のカ—ドを{End}");
+u8 GetItemNote_098_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_099_000[] = _("コレクションナンバ— 100 のカ—ドを{End}");
+u8 GetItemNote_099_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_100_000[] = _("ひろしのファミリ—スキル【ランチャ—】を{End}");
+u8 GetItemNote_100_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_101_000[] = _("ひろしのファミリ—スキル【くつしたボム】を{End}");
+u8 GetItemNote_101_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_102_000[] = _("ひろしのファミリ—スキル【ゲッチュ—！】を{End}");
+u8 GetItemNote_102_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_103_000[] = _("ひろしのファミリ—スキル【ひろしＳＵＮ】を{End}");
+u8 GetItemNote_103_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_104_000[] = _("みさえのファミリ—スキル【ヒップドロップ】を{End}");
+u8 GetItemNote_104_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_105_000[] = _("みさえのファミリ—スキル【げんこつ】を{End}");
+u8 GetItemNote_105_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_106_000[] = _("みさえのファミリ—スキル【セキトリス—ツ】を{End}");
+u8 GetItemNote_106_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_107_000[] = _("みさえのファミリ—スキル{End}");
+u8 GetItemNote_107_001[] = _("【まほうしょうじょミサリン】をてにいれた{End}");
+
+u8 GetItemNote_108_000[] = _("ひまわりのファミリ—スキル【はいはい】を{End}");
+u8 GetItemNote_108_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_109_000[] = _("ひまわりのファミリ—スキル{End}");
+u8 GetItemNote_109_001[] = _("【おんせんせんし】を てにいれた{End}");
+
+u8 GetItemNote_110_000[] = _("ひまわりのファミリ—スキル【ちょうちょ】を{End}");
+u8 GetItemNote_110_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_111_000[] = _("シロのファミリ—スキル【あなほり】を{End}");
+u8 GetItemNote_111_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_112_000[] = _("シロのファミリ—スキル【かみつき】を{End}");
+u8 GetItemNote_112_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_113_000[] = _("シロのファミリ—スキル【わたあめ】を{End}");
+u8 GetItemNote_113_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_114_000[] = _("アイテム【ブランドもののバッグ】を{End}");
+u8 GetItemNote_114_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_115_000[] = _("アイテム【フィギュア】を{End}");
+u8 GetItemNote_115_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_116_000[] = _("アイテム【えいかいわセット】を{End}");
+u8 GetItemNote_116_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_117_000[] = _("アイテム【バレ—ボ—ル】を{End}");
+u8 GetItemNote_117_001[] = _("てにいれた{End}");
+
+u8 GetItemNote_118_000[] = _("アイテム【アイちゃんのしゃしん】を{End}");
+u8 GetItemNote_118_001[] = _("てにいれた{End}");
---
+++ strings/NameList_jp.c
@@ -0,0 +1,42 @@
+#include "libgba\gba_types.h"
+#include "strings\NameList.h"
+
+// text 0x080B1B88 - 0x080B1D61
+u8 NameList_000[] = _("しんのすけ{End}");
+u8 NameList_001[] = _("ひろし{End}");
+u8 NameList_002[] = _("みさえ{End}");
+u8 NameList_003[] = _("ひまわり{End}");
+u8 NameList_004[] = _("シロ{End}");
+u8 NameList_005[] = _("マサオくん{End}");
+u8 NameList_006[] = _("カザマくん{End}");
+u8 NameList_007[] = _("ネネちゃん{End}");
+u8 NameList_008[] = _("ボ—ちゃん{End}");
+u8 NameList_009[] = _("マサオママ{End}");
+u8 NameList_010[] = _("カザマママ{End}");
+u8 NameList_011[] = _("ネネママ{End}");
+u8 NameList_012[] = _("ヨシリン&ミッチ—{End}");
+u8 NameList_013[] = _("ヨシリン&ミッチ—{End}");
+u8 NameList_014[] = _("よしながせんせい{End}");
+u8 NameList_015[] = _("まつざかせんせい{End}");
+u8 NameList_016[] = _("えんちょうせんせい{End}");
+u8 NameList_017[] = _("あげおせんせい{End}");
+u8 NameList_018[] = _("きたかすかべはかせ{End}");
+u8 NameList_019[] = _("リリこ{End}");
+u8 NameList_020[] = _("ルル{End}");
+u8 NameList_021[] = _("おおぶくろはかせ{End}");
+u8 NameList_022[] = _("アンジェラ{End}");
+u8 NameList_023[] = _("せんちょう{End}");
+u8 NameList_024[] = _("せんいん{End}");
+u8 NameList_025[] = _("じょうきゃく{End}");
+u8 NameList_026[] = _("おばちゃん{End}");
+u8 NameList_027[] = _("おそばやさん{End}");
+u8 NameList_028[] = _("シネマランドじゅうぎょういん{End}");
+u8 NameList_029[] = _("スンノケシおうじ{End}");
+u8 NameList_030[] = _("ハイグレまおう{End}");
+u8 NameList_031[] = _("パラダイスキング{End}");
+u8 NameList_032[] = _("ななこ{End}");
+u8 NameList_033[] = _("しのぶ{End}");
+u8 NameList_034[] = _("しはいにん{End}");
+
+
+
