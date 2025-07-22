f605c83d08fffa28c8a37061e441bc35c2afe7c1
GitHub Wokann <Kenny_chen1998@outlook.com>
2025-07-22 13:56:11 +0800

    老函数

--- src/HookInOrigin/renderText.s
+++ src/HookInOrigin/renderText.s
@@ -3,3 +3,23 @@
     ldr R0,=ExpandChineseFontAddress
     mov pc,R0
 .pool
+
+
+//.org UnlockCharmapLimit
+//    mov r6,0
+/* 
+E0 26 movs r6,0xe0 ->
+00 26 movs r6,0
+*/
+
+//.org FontGlyphAddress
+//.word 0x09000000
+/* 
+34 7D 0A 08->
+00 00 00 09
+*/
+
+//.org ExpandChineseFontJump
+//    ldr r0,=ExpandChineseFontAddress
+//    mov pc,r0
+//.pool
